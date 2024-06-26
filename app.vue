ac902e69df2694e598b92a301cdb8a8f414fc5da
BoneDeath <mhasannudin300@gmail.com>
2024-06-26 14:26:14 +0700

    further changess

--- app.vue
+++ app.vue
@@ -1,5 +1,96 @@
 <template>
     <div>
-        <h2 class="text-red-600 2xl:text-teal-400">Heading 2</h2>
+        <section class="flex flex-col h-[30rem] items-center justify-center place-items-center" style="background-image: url('header-sampul-bengkel-print-care.png'); background-repeat: no-repeat; background-size: cover;">
+            <div class="container flex flex-col items-center justify-center px-0 md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <img src="public/Logo%20Bengkel%20Print%20Putih.png" class="h-20">
+            </div>
+            <div class="container flex flex-col items-center justify-center mt-16 px-0 md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <h2 class="font-semibold text-4xl text-slate-50">Cek Garansi</h2>
+                <span class="text-center text-slate-50 text-xl sm:text-center">Cari tau dengan syarat dan ketentuan klaim garansi produk Riecat</span>
+            </div>
+            <div class="container pt-9 px-0 md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <div class="bg-slate-100 flex flex-row m-3 rounded-full">
+                    <div class="bg-white flex items-center justify-center pl-6 pr-3 py-3 rounded-l-full">
+                        <img src="public/cropped-favicon.png" class="w-10">
+                    </div>
+                    <div class="flex rounded-r-full w-full">
+                        <input class="font-semibold h-full outline-none p-4 rounded-r-full shadow-sm text-lg text-slate-500 uppercase w-full" placeholder="SERIAL NUMBER">
+                    </div>
+                    <div class="flex items-center justify-center pl-3 pr-6 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="h-7 text-sky-500 text-sm w-7">
+                            <g>
+                                <path fill="none" d="M0 0h24v24H0z"/>
+                                <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="flex flex-col h-auto place-items-center">
+            <div class="container flex flex-col-reverse px-0 py-4 md:flex-row md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <div class="p-6 w-full md:w-7/12">
+                    <h3 class="font-bold mt-12 text-xl">Layanan After Sales!</h3>
+                    <p class="mt-3">Maksimalkan kinerja mesin dengan after sales kami</p>
+                    <p class="mt-6">Aktifkan serial number mesin anda dan rasakan kenyamanan memiliki produk dari Bengkel Print. Serial yang terdaftar adalah mesin Riecat.</p>
+                    <div class="flex mt-8">
+                        <div class="flex flex-col items-center justify-center w-4/12">
+                            <img src="public/icon-mesin-dtf-bengkel-print-care.png" class="w-36">
+                            <p class="font-bold italic text-stone-500">Printer Alfa</p>
+                        </div>
+                        <div class="flex flex-col items-center justify-center w-4/12">
+                            <img src="public/icon-mesin-dtg-bengkel-print-care.png" class="w-36">
+                            <p class="font-bold italic text-stone-500">Printer DTG</p>
+                        </div>
+                        <div class="flex flex-col items-center justify-center w-4/12">
+                            <img src="public/icon-mesin-uv-bengkel-print-care.png" class="w-36">
+                            <p class="font-bold italic text-stone-500">Printer UV</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-6 text-right w-full sm:w-full md:w-5/12">
+                    <span>Belum tau tempat</span>
+                    <br>
+                    <span>Serial mesin anda?<a href="" class="font-bold text-sky-400"> Lihat disini</a></span>
+                    <div class="flex justify-center p-6">
+                        <img src="public/mesin-uv-bengkel-print-care.png" class="w-80">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-sky-100 flex flex-col h-auto place-items-center">
+            <div class="container flex flex-col px-0 py-4 md:flex md:flex-row md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <div class="md:w-6/12 p-6 w-full">
+                    <div class="flex justify-center p-6">
+                        <img src="public/icon-customer-servis-1024x638.png" class="w-64 md:w-full">
+                    </div>
+                </div>
+                <div class="flex items-center justify-center p-6 w-full md:w-6/12">
+                    <div class="text-center">
+                        <h3 class="font-bold mt-12 text-xl">Penanganan tepat dan ter-arah!</h3>
+                        <p class="mt-3">Support Teknisi Bengkel Print Pusat</p>
+                        <p class="mt-6">Untuk penanganan tepat dan ter-arah, hubungi support teknisi dengan nomor WA/ chat admin web kami.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-white flex flex-col h-auto place-items-center">
+            <div class="container flex flex-col px-0 py-4 md:flex md:flex-row md:px-8 lg:px-16 xl:px-36 2xl:px-40">
+                <div class="md:w-6/12 p-6 w-full">
+                    <div class="flex flex-col items-center justify-center p-6">
+                        <img src="public/Icon-Garansi-Diterima.png" class="mb-5 w-32">
+                        <h4 class="font-semibold text-3xl text-center text-sky-500">Garansi Ter-Klaim</h4>
+                        <p class="mt-3 text-center text-lg">Beberapa hal yang harus diperhatikan dalam melakukan klaim garansi mesin dengan baik</p>
+                    </div>
+                </div>
+                <div class="md:w-6/12 p-6 w-full">
+                    <div class="flex flex-col items-center justify-center p-6">
+                        <img src="public/Icon-Garansi-Di-Tolak.png" class="mb-5 w-32" style="filter: hue-rotate(160deg);">
+                        <h4 class="font-semibold text-3xl text-center text-sky-500" style="filter: hue-rotate(160deg);">Garansi Di-Tolak</h4>
+                        <p class="mt-3 text-center text-lg">Beberapa hal yang harus di perhatikan jika garansi mesin mengalami penolakan dari pusat</p>
+                    </div>
+                </div>
+            </div>
+        </section>         
     </div>
 </template>
